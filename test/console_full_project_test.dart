5a0a737cb09d9c5211cf06c7d36a8a93933bd9c4
Victor Gheiler <victorgheiler1@Orlys-iMac-Pro.local>
2024-08-27 15:14:19 -0400

    added unit tests for calc

--- test/console_full_project_test.dart
+++ test/console_full_project_test.dart
@@ -1,8 +1,28 @@
 import 'package:console_full_project/console_full_project.dart';
+import 'package:console_full_project/src/calculate.dart';
 import 'package:test/test.dart';
 
 void main() {
   test('calculate', () {
     expect(calculate(), 42);
   });
+
+  test('add', () {
+    expect(calculateAdd(), 69);
+  });
+
+    test('subtract', () {
+    expect(calculateSubtract(), 69);
+  });
+
+    test('multiply', () {
+    expect(calculateMultiply(), 69);
+  });
+
+    test('divide', () {
+    expect(calculateDivide(), 69.0);
+  });
+
+
+
 }
